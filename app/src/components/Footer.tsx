6bdac0b21d4cc2d64c682688660b8b809c277831
antydizajn <danveld@gmail.com>
2026-03-20 13:55:31 +0100

    fix(ui): add missing Link import in Footer.tsx

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
